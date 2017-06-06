e967e83c5c3dd0e935a6f5b1fb100d1fbc5f6003
David Timmons <github@timmons.io>
2017-06-06 00:08:49 -0500

    Create utility to scrape and download data from image URLs.

---
+++ utilities/scrape_image_urls.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+"""scrape_image_urls.py
+
+Scrape all image URLs found in the <../content/blog> directory then download all images into
+the <../content/static/img> directory.
+"""
+
+import os
+import re
+import shutil
+import urllib.parse
+import urllib.request
+
+
+def download_images(images, image_directory):
+    """Download all images and save to the local file system.
+
+    Args:
+        images: List containing image URL strings.
+        image_directory: String describing the directory where images should be saved.
+    """
+    for image in images:
+        image_name = urllib.parse.urlparse(image).path.split('/')[-1]
+        image_path = os.path.join(image_directory, image_name)
+        with urllib.request.urlopen(image) as in_file, open(image_path, 'wb') as out_file:
+            shutil.copyfileobj(in_file, out_file)
+
+
+def find_image_urls(haystack):
+    """Returns a list of all matching image URLs in a string argument.
+
+    Args:
+        haystack: String through which to search for image URLs.
+
+    Returns:
+        List of image URL strings.
+    """
+    needle = re.compile('(http.*\.)(gif|jpg|jpeg|png|svg|tif)', re.IGNORECASE)
+    matches = [''.join(m) for m in needle.findall(haystack)]
+    return matches
+
+
+def search_content_files(content_directory, image_directory):
+    """Opens and searches all content files for image URLs.
+
+    Args:
+        content_directory: String describing where the content files are stored.
+        image_directory: String describing where the image files should be saved.
+    """
+    for dirpath, dirnames, filenames in os.walk(content_directory):
+        for filename in filenames:
+            content_file = os.path.join(dirpath, filename)
+            with open(content_file, 'r') as f:
+                images = find_image_urls(f.read())
+                download_images(images, image_directory)
+
+
+def main():
+    """Searches content files for image URLs then downloads them to a local directory.
+    """
+    path_prefix = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+    content_directory = os.path.join(path_prefix, 'content', 'blog')
+    image_directory = os.path.join(path_prefix, 'content', 'static', 'img')
+    search_content_files(content_directory, image_directory)
+
+
+if __name__ == '__main__':
+    main()
